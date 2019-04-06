2a171c78d8290b4b48864b74901e9f8695d6129c
Gianfranco Palumbo <gianpa@gmail.com>
2019-04-06 20:20:55 +0300

    add Fab button

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Grid, Segment } from 'semantic-ui-react';
-import { ReactiveBase, ToggleButton } from '@appbaseio/reactivesearch';
+import React, { Component } from 'react';
+import { Grid, Segment, Button } from 'semantic-ui-react';
+import { ReactiveBase } from '@appbaseio/reactivesearch';
 import { ReactiveMap } from '@appbaseio/reactivemaps';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 import CategoryMenu from './Top/CategoryMenu';
 import './Main.scss';
 
-const renderLeftCol = (hits, streamHits, loadMore, renderMap, renderPagination) => (
-  <div style={{ display: 'flex' }}>
+export default class Main extends Component {
+  state = {
+    // adding | editing | singleResult | multiResults | browsing
+    mode: 'browsing',
+  };
+
+  renderResults = hits => (
     <div className="card-container">
       {hits.map(data => (
         <div key={data._id} className="card">
@@ -23,59 +30,83 @@ const renderLeftCol = (hits, streamHits, loadMore, renderMap, renderPagination) 
           </div>
         </div>
       ))}
-      {/* {renderPagination()} */}
     </div>
-    <div className="map-container">{renderMap()}</div>
-  </div>
-);
+  );
 
-export default () => (
-  <div className="container">
-    <ReactiveBase
-      app="helpmap"
-      credentials="6Oc2N0Ats:cd4782b5-de89-4675-9a48-e4b5423cd9e2"
-      // type="listing"
-      theme={{
-        colors: {
-          primaryColor: '#fff',
-        },
-      }}>
-      <Grid.Row className="top-row">
-        <Grid.Column>
-          <Segment basic>
-            <CategoryMenu />
-          </Segment>
-        </Grid.Column>
-      </Grid.Row>
-      <ReactiveMap
-        componentId="map"
-        dataField="location"
-        className="right-col"
-        defaultZoom={13}
-        defaultCenter={{ lat: 49.8397, lng: 24.0297 }} // Lviv
-        defaultMapStyle="Blue Essence"
-        // pagination
-        // onPageChange={() => {
-        //   window.scrollTo(0, 0);
-        // }}
-        // onPopoverClick={this.onPopoverClick}
-        showMarkerClusters={false}
-        showSearchAsMove
-        searchAsMove
-        // showMapStyles={true}
-        unit="km"
-        onAllData={renderLeftCol}
-        // onData={data => ({
-        //   label: data.types.map((type, i) => (
-        //     <span key={i} style={{ width: 40, display: 'block', textAlign: 'center' }}>
-        //       {type}
-        //     </span>
-        //   )),
-        // })}
-        react={{
-          and: ['GuestSensor', 'Types', 'search'],
-        }}
-      />
-    </ReactiveBase>
-  </div>
-);
+  renderLeftCol = (hits, streamHits, loadMore, renderMap, renderPagination) => (
+    <div style={{ display: 'flex' }}>
+      {hits.length > 0 && this.renderResults(hits)}
+      {/* {renderPagination()} */}
+      <div className="map-container">{renderMap()}</div>
+    </div>
+  );
+
+  addNewPlace = () => {
+    this.setState({ mode: 'adding' });
+  };
+
+  // FSgW29GYr:1f6ad732-faf2-4466-aa4b-4a1f35fd09d3
+  renderFloatingButton = () => (
+    <Fab onClick={this.addNewPlace} className="fab" aria-label="Add Location" disableRipple color="primary">
+      <AddIcon />
+    </Fab>
+  );
+
+  onPopoverClick = () => {
+    this.setState({ mode: 'singleResult' });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <ReactiveBase
+          app="helpmap"
+          // analytics
+          credentials="6Oc2N0Ats:cd4782b5-de89-4675-9a48-e4b5423cd9e2"
+          // type="listing"
+          theme={{
+            colors: {
+              primaryColor: '#fff',
+            },
+          }}>
+          <Grid.Row className="top-row">
+            <Grid.Column>
+              <Segment basic>
+                <CategoryMenu />
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+          <ReactiveMap
+            componentId="map"
+            dataField="location"
+            className="right-col"
+            defaultZoom={13}
+            defaultCenter={{ lat: 49.8397, lng: 24.0297 }} // Lviv
+            defaultMapStyle="Blue Essence"
+            // pagination
+            // onPageChange={() => {
+            //   window.scrollTo(0, 0);
+            // }}
+            // onPopoverClick={this.onPopoverClick}
+            showMarkerClusters={false}
+            // autoClosePopover
+            showSearchAsMove
+            searchAsMove
+            // showMapStyles={true}
+            unit="km"
+            onAllData={this.renderLeftCol}
+            // onData={data => ({
+            //   label: data.types.map((type, i) => (
+            //     <span key={i} style={{ width: 40, display: 'block', textAlign: 'center' }}>
+            //       {type}
+            //     </span>
+            //   )),
+            // })}
+            react={{ and: ['GuestSensor', 'Types', 'search'] }}
+          />
+          {this.renderFloatingButton()}
+        </ReactiveBase>
+      </div>
+    );
+  }
+}
