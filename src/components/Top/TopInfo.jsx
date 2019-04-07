f3e7be79214a9bfd6d9fcc2e84629951df47b450
Gianfranco Palumbo <gianpa@gmail.com>
2019-04-07 15:36:01 +0300

    improve language switcher

--- src/components/Top/TopInfo.jsx
+++ src/components/Top/TopInfo.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Segment, Modal, Header } from 'semantic-ui-react';
+import { Modal, Header } from 'semantic-ui-react';
 import { injectIntl, FormattedHTMLMessage } from 'react-intl';
 import LanguageSwitch from './LanguageSwitch';
 import InfoIcon from '../../assets/info';
 import Logo from '../../assets/logo';
 
+import './TopInfo.scss';
+
 const TopPanel = ({ intl }) => {
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      {/* cursor pointer */}
       <Modal trigger={<InfoIcon />} closeIcon>
         <Modal.Content>
           <FormattedHTMLMessage id="Info" />
@@ -17,9 +20,9 @@ const TopPanel = ({ intl }) => {
         <Logo />
         <Header.Subheader>{intl.formatMessage({ id: 'Tagline' })}</Header.Subheader>
       </Header>
-      <Segment.Inline>
+      <div className="language-switch">
         <LanguageSwitch />
-      </Segment.Inline>
+      </div>
     </div>
   );
 };
