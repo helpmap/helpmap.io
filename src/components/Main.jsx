48a11b9456ad09b4f367cb078f19ed294495d2a6
Gianfranco Palumbo <gianpa@gmail.com>
2019-04-08 12:14:41 +0300

    fix Adding item using outside component with ReactiveList

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-bind */
 import React, { useState } from 'react';
 import { Grid, Segment } from 'semantic-ui-react';
-import { ReactiveBase } from '@appbaseio/reactivesearch';
+import { ReactiveBase, ReactiveList } from '@appbaseio/reactivesearch';
 import { ReactiveMap } from '@appbaseio/reactivemaps';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
@@ -18,54 +18,26 @@ const Main = () => {
   const [result, setResult] = useState({});
   // const [modalOpen, handleModal] = useState(false);
 
-  const renderResults = hits => {
-    return hits.map(data => (
+  function renderItem(data) {
+    if (data.length < 1) return <h2>No results</h2>;
+    return (
       <Card key={data._id}>
-        <div className="card__image" style={{ backgroundImage: `url(${data.image})` }} title={data.name} />
+        {/* <div className="card__image" style={{ backgroundImage: `url(${data.image})` }} title={data.name} /> */}
         <div>
           <h2>{data.name}</h2>
-          {/* {data.types.map((type, i) => (
-            <div key={i} className="card__type">
-              {type}
-            </div>
-          ))} */}
           <a
             target="_blank"
             rel="noopener noreferrer"
             href={`http://maps.google.com/?ie=UTF8&hq=&ll=${data.location.lat},${data.location.lon}&z=13`}>
             {data.address}
           </a>
-          {/* <p className="card__description">{data.description}</p> */}
         </div>
       </Card>
-    ));
-  };
+    );
+  }
 
   const renderLeftCol = (hits, streamHits, loadMore, renderMap, renderPagination) => {
-    return (
-      <Grid padded="horizontally">
-        <Grid.Row style={{ padding: 0 }}>
-          {show && (
-            <Grid.Column className="left-col" width={4}>
-              {mode === 'multiResults' &&
-                (hits.length > 0 ? (
-                  renderResults(hits)
-                ) : (
-                  // <h2>{injectIntl.formatMessage({ id: 'no_results' })}</h2>
-                  <h2>No results</h2>
-                ))}
-              {mode === 'adding' && <AddMenu setShow={setShow} setMode={setMode} />}
-              {mode === 'singleResult' && hits.length > 0 && (
-                <AddMenu data={result} setShow={setShow} setMode={setMode} />
-              )}
-            </Grid.Column>
-          )}
-          <Grid.Column className="map-container" width={show ? 12 : 16}>
-            {renderMap()}
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    );
+    return renderMap();
   };
 
   // FSgW29GYr:1f6ad732-faf2-4466-aa4b-4a1f35fd09d3
@@ -113,36 +85,57 @@ const Main = () => {
             </Segment>
           </Grid.Column>
         </Grid.Row>
-        <ReactiveMap
-          componentId="map"
-          dataField="location"
-          className="right-col"
-          style={{ height: '100%', padding: 0 }}
-          defaultZoom={13}
-          defaultCenter={{ lat: 49.8397, lng: 24.0297 }} // Lviv
-          defaultMapStyle="Flat Map"
-          // pagination
-          // onPageChange={() => {
-          //   window.scrollTo(0, 0);
-          // }}
-          onPopoverClick={onPopoverClick}
-          showMarkerClusters={false}
-          // autoClosePopover
-          showSearchAsMove
-          searchAsMove
-          // showMapStyles={true}
-          unit="km"
-          onAllData={renderLeftCol}
-          onData={_ => ({ icon: '/pin.svg' })}
-          // onData={data => ({
-          //   label: data.types.map((type, i) => (
-          //     <span key={i} style={{ width: 40, display: 'block', textAlign: 'center' }}>
-          //       {type}
-          //     </span>
-          //   )),
-          // })}
-          react={{ and: ['Types'] }}
-        />
+        <Grid padded="horizontally">
+          <Grid.Row style={{ padding: 0 }}>
+            {show && (
+              <Grid.Column className="left-col" width={4}>
+                {mode === 'multiResults' && (
+                  <ReactiveList
+                    react={{ and: ['Types'] }}
+                    componentId="SearchResult"
+                    dataField=""
+                    renderItem={renderItem}
+                  />
+                )}
+                {mode === 'adding' && <AddMenu setShow={setShow} setMode={setMode} />}
+                {mode === 'singleResult' && <AddMenu data={result} setShow={setShow} setMode={setMode} />}
+              </Grid.Column>
+            )}
+            <Grid.Column className="map-container" width={show ? 12 : 16}>
+              <ReactiveMap
+                componentId="map"
+                dataField="location"
+                className="right-col"
+                style={{ height: '100%', padding: 0 }}
+                defaultZoom={13}
+                defaultCenter={{ lat: 49.8397, lng: 24.0297 }} // Lviv
+                defaultMapStyle="Flat Map"
+                // pagination
+                // onPageChange={() => {
+                //   window.scrollTo(0, 0);
+                // }}
+                onPopoverClick={onPopoverClick}
+                showMarkerClusters={false}
+                // autoClosePopover
+                showSearchAsMove
+                searchAsMove
+                // showMapStyles={true}
+                unit="km"
+                onAllData={renderLeftCol}
+                // eslint-disable-next-line no-unused-vars
+                onData={_ => ({ icon: '/pin.svg' })}
+                // onData={data => ({
+                //   label: data.types.map((type, i) => (
+                //     <span key={i} style={{ width: 40, display: 'block', textAlign: 'center' }}>
+                //       {type}
+                //     </span>
+                //   )),
+                // })}
+                react={{ and: ['Types'] }}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </ReactiveBase>
       {renderFloatingButton()}
     </div>
