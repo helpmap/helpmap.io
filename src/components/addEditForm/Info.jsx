bc683bde8669eeb9b663b926eb9ab815582c0154
Gianfranco Palumbo <gianpa@gmail.com>
2019-04-13 12:09:46 +0300

    Info: translate categories

--- src/components/addEditForm/Info.jsx
+++ src/components/addEditForm/Info.jsx
@@ -3,19 +3,20 @@ import { Container } from 'semantic-ui-react';
 import BackIcon from '@material-ui/icons/ArrowBack';
 import EditIcon from '@material-ui/icons/Edit';
 import { Card } from 'antd';
+import { injectIntl } from 'react-intl';
 
 import './SideMenu.scss';
 import categories from '../Top/messages/menuMessages';
 import { appbaseRef } from '../Main';
 
-const showCategories = types =>
+const showCategories = (types, intl) =>
   types.split(' ').map((name, i) => (
-    <span key={i} className="category-icon" title={name}>
+    <span key={i} className="category-icon" title={intl.formatMessage({ id: categories[name].id })}>
       {categories[name].icon}
     </span>
   ));
 
-const Info = ({ setMode, id }) => {
+const Info = ({ setMode, id, intl }) => {
   const [data, setData] = useState();
 
   useEffect(() => {
@@ -43,11 +44,11 @@ const Info = ({ setMode, id }) => {
           href={`https://www.google.com/maps/dir/?api=1&destination=${data.location.lat},${data.location.lon}`}>
           {data.address}
         </a>
-        <div className="category-menu">{data.types && showCategories(data.types[0])}</div>
+        <div className="category-menu">{data.types && showCategories(data.types[0], intl)}</div>
         <p className="description">{data.description}</p>
       </Card>
     </Container>
   );
 };
 
-export default Info;
+export default injectIntl(Info);
