6fdc04632949e0959874fba2c8bfbb19e4802f27
Gianfranco Palumbo <gianpa@gmail.com>
2019-05-20 18:02:28 +0300

    sharing

--- src/assets/info.js
+++ src/assets/info.js
@@ -11,8 +11,7 @@ const SvgComponent = props => (
     stroke="currentColor"
     strokeWidth="2"
     strokeLinecap="round"
-    strokeLinejoin="round"
-    className="feather feather-info">
+    strokeLinejoin="round">
     <circle cx="12" cy="12" r="10" />
     <line x1="12" y1="16" x2="12" y2="12" />
     <line x1="12" y1="8" x2="12" y2="8" />
--- src/components/addEditForm/Info.jsx
+++ src/components/addEditForm/Info.jsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Container, Button, Form } from 'semantic-ui-react';
 import BackIcon from '@material-ui/icons/ArrowBack';
 import EditIcon from '@material-ui/icons/Edit';
+import ShareIcon from '@material-ui/icons/Share';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import Slide from '@material-ui/core/Slide';
 import { Card } from 'antd';
 import { injectIntl } from 'react-intl';
 
 import './SideMenu.scss';
 import categories from '../Top/messages/menuMessages';
 import { appbaseRef } from '../Main';
+
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
 
 const showCategories = (types, intl) =>
   types.split(' ').map((name, i) => (
@@ -16,8 +25,9 @@ const showCategories = (types, intl) =>
     </span>
   ));
 
-const Info = ({ setMode, backToResults, id, intl }) => {
+let Info = ({ setMode, backToResults, id, intl }) => {
   const [data, setData] = useState();
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,27 +38,80 @@ const Info = ({ setMode, backToResults, id, intl }) => {
     if (id) fetchData();
   }, [id]);
 
+  function share() {
+    // open modal
+    setShowModal(true);
+    setTimeout(() => {
+      const url = `https://helpmap.io/id/${id}`;
+      if (document.getElementById('url-text')) document.getElementById('url-text').value = url;
+    }, 300);
+  }
+
+  function copyShareLink() {
+    try {
+      if (document.getElementById('url-text')) document.getElementById('url-text').select();
+      document.execCommand('copy');
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
+  }
+
+  function handleClose() {
+    setShowModal(false);
+  }
+
   if (!data) return null;
 
   return (
-    <Container text fluid className="info-container">
-      <Card bordered={false}>
-        {/* <Button icon="edit" className="edit-btn" onClick={() => setMode('editing')} /> */}
-        {/* <Button icon="arrow left" className="back-btn" onClick={() => setMode('multiResults')} /> */}
-        <EditIcon className="edit-btn" onClick={() => setMode('editing')} />
-        <BackIcon className="back-btn" onClick={backToResults} />
-        <h2>{data.name}</h2>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={`https://www.google.com/maps/dir/?api=1&destination=${data.location.lat},${data.location.lon}`}>
-          {data.address}
-        </a>
-        <div className="category-menu">{data.types && showCategories(data.types[0], intl)}</div>
-        <p className="description">{data.description}</p>
-      </Card>
-    </Container>
+    <>
+      <Container text fluid className="info-container">
+        <Card bordered={false}>
+          {/* <Button icon="edit" className="edit-btn" onClick={() => setMode('editing')} /> */}
+          {/* <Button icon="arrow left" className="back-btn" onClick={() => setMode('multiResults')} /> */}
+          <>
+            <EditIcon className="edit-btn" onClick={() => setMode('editing')} />
+            <ShareIcon className="share-btn" onClick={() => share()} />
+            <BackIcon className="back-btn" onClick={backToResults} />
+          </>
+          <h2>{data.name}</h2>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`https://www.google.com/maps/dir/?api=1&destination=${data.location.lat},${data.location.lon}`}>
+            {data.address}
+          </a>
+          <div className="category-menu">{data.types && showCategories(data.types[0], intl)}</div>
+          <p className="description">{data.description}</p>
+        </Card>
+      </Container>
+      <Dialog
+        TransitionComponent={Transition}
+        onClose={handleClose}
+        keepMounted
+        open={showModal}
+        fullWidth
+        aria-labelledby="share">
+        <DialogContent>
+          <Form className="form-inline">
+            <div className="form-group">
+              <Form.TextArea className="form-control" id="url-text" rows={1} readOnly />
+              {/* <textarea className="form-control" id="url-text" rows={1} readOnly /> */}
+            </div>
+            <Button onClick={copyShareLink}>Copy</Button>
+          </Form>
+        </DialogContent>
+        {/* <div className={classes.paper}></div> */}
+      </Dialog>
+    </>
   );
+};
+
+Info.propTypes = {
+  setMode: PropTypes.func,
+  backToResults: PropTypes.func,
+  id: PropTypes.string,
+  intl: PropTypes.any,
 };
 
 export default injectIntl(Info);
