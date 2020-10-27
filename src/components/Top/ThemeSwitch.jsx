e6734548ebc84115f8430fedbd6a8d5b629e2ed2
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2020-10-27 18:31:33 +0000

    Add constructor and toggleDarkMode method

--- src/components/Top/ThemeSwitch.jsx
+++ src/components/Top/ThemeSwitch.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import './ThemeSwitch.scss';
 
 class ThemeSwitch extends React.Component {
-  onClickBtn() {
+  constructor(props) {
+    super(props);
+    this.toggleDarkMode = this.toggleDarkMode.bind(this);
+  }
+
+  toggleDarkMode() {
     //Toggle dark mode on and off.
-    console.log('The button has been clicked');
+    console.log('Dark mode has been toggled');
   }
 
   render() {
     return (
       <label className="theme-switch">
-        <input type="checkbox" />
+        <input type="checkbox" onClick={this.toggleDarkMode} />
         <span className="slider round" />
       </label>
     );
