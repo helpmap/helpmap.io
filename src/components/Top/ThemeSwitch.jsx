cf9381ab27199d9400b2391ff3620e898b99c967
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2020-10-28 23:32:31 +0000

    Add darkMode state

--- src/components/Top/ThemeSwitch.jsx
+++ src/components/Top/ThemeSwitch.jsx
@@ -5,11 +5,16 @@ class ThemeSwitch extends React.Component {
   constructor(props) {
     super(props);
     this.toggleDarkMode = this.toggleDarkMode.bind(this);
+    this.state = {
+      darkMode: false,
+    };
   }
 
   toggleDarkMode() {
-    //Toggle dark mode on and off.
-    console.log('Dark mode has been toggled');
+    //Toggle dark mode on and off
+    this.state.darkMode == true ? this.setState({ darkMode: false }) : this.setState({ darkMode: true });
+
+    console.log(this.state.darkMode);
   }
 
   render() {
